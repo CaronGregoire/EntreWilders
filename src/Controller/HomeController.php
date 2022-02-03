19d2467c8445a61b7adc71f0aac235326c8e5072
CaronGregoire <gregoirecaron33@gmail.com>
2022-02-03 10:39:18 +0100

    add composer fixture and fix error twig

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +13,12 @@ class HomeController extends AbstractController
     /**
      * @Route("/home", name="home")
      */
-    public function index(): Response
+    public function index(ManagerRegistry $managerRegistry): Response
     {
+        $userRepository = $managerRegistry->getRepository(User::class);
+        $users = $userRepository->findAll();
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'users' => '$users',
         ]);
     }
 }
